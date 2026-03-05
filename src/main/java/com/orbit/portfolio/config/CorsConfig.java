00c5d884e1ed01239946759653a6697401e60d23
varmanamish <varmanamish@gmail.com>
2026-03-06 00:52:51 +0530

    Restricted Origins

--- src/main/java/com/orbit/portfolio/config/CorsConfig.java
+++ src/main/java/com/orbit/portfolio/config/CorsConfig.java
@@ -10,8 +10,10 @@ public class CorsConfig implements WebMvcConfigurer{
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("http://localhost:3000", "http://localhost:5173", "https://portfolio-manager-production.up.railway.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*");
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
     }
 }
